25b63385c3a188fe05e84f42523d53991b80182e
Mohamed Berrachdi <mohamed.berrachdi@onclusive.com>
2025-07-18 11:16:45 +0100

    Add first version of job monitor starter

---
+++ src/main/java/org/medtech/springbootjobmonitor/storage/JobLogStorage.java
@@ -0,0 +1,18 @@
+package org.medtech.springbootjobmonitor.storage;
+
+import org.medtech.springbootjobmonitor.model.JobExecutionLog;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Interface for storing and retrieving job execution logs.
+ * This interface defines methods to add logs for a specific job,
+ * retrieve logs for a specific job, and get all job names
+ * that have logs stored.
+ */
+public interface JobLogStorage {
+    void addLog(String jobName, JobExecutionLog log);
+    List<JobExecutionLog> getLogsForJob(String jobName);
+    Map<String, List<JobExecutionLog>> getAllLogs();
+}
